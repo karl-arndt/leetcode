0d5dd1d1ccce9766edfb2d3a8b912fe3f2122a38
karl-arndt <157114758+karl-arndt@users.noreply.github.com>
2025-07-21 15:56:10 -0400

    Add 1. Two Sum

--- Java/Easy/0001-two-sum.java
+++ Java/Easy/0001-two-sum.java
@@ -3,6 +3,17 @@ import java.util.HashMap;
 class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        return nums;
+        int[] ans = new int[2];
+        int compliment;
+        for (int i = 0; i < nums.length; i++) {
+            compliment = target - nums[i];
+            if (map.containsKey(compliment)) {
+                ans[0] = map.get(compliment);
+                ans[1] = i;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return ans;
     }
 }
