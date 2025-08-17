d6a6f5d3f9bc0081e0aa1421329de8161ca6ec45
karl-arndt <157114758+karl-arndt@users.noreply.github.com>
2025-08-17 03:32:38 -0400

    Create 0121-best-time-to-buy-and-sell-stock.java

---
+++ Java/Easy/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,15 @@
+class BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int profit = 0;
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            }
+            if (price - minPrice > profit) {
+                profit = price - minPrice;
+            }
+        }
+        return profit;
+    }
+}
