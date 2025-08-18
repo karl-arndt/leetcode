50f8e10db9c665c7efb4877ce21b75e76c3119ae
karl-arndt <157114758+karl-arndt@users.noreply.github.com>
2025-08-18 07:05:54 -0400

    Update 0001-two-sum.js

--- JavaScript/Easy/0001-two-sum.js
+++ JavaScript/Easy/0001-two-sum.js
@@ -4,5 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    
+    let map = {};
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (complement in map) {
+            return [map[complement], i];
+        } else {
+            map[nums[i]] = i;
+        }
+    }
 };
