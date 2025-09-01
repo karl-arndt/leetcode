d21850e422b09b9f1f925d120908147f4439a981
karl-arndt <157114758+karl-arndt@users.noreply.github.com>
2025-09-01 10:32:17 -0400

    Update 0001-two-sum.py

--- Python/Easy/0001-two-sum.py
+++ Python/Easy/0001-two-sum.py
@@ -1,2 +1,10 @@
-class Solution:
+class TwoSum:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        res = []
+        for index, num in enumerate(nums):
+            compliment = target - num
+            if compliment in seen:
+                return [seen[compliment], index]
+            seen[num] = index
+        return res
