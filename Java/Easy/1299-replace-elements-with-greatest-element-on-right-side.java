47e4c431829edc5e4b4b4906cd20f72d9f19ccab
karl-arndt <157114758+karl-arndt@users.noreply.github.com>
2026-01-04 08:27:37 -0500

    Create 1299-replace-elements-with-greatest-element-on-right-side.java

---
+++ Java/Easy/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -0,0 +1,14 @@
+class ReplaceElementsWithGreatestElementOnRightSide {
+    public int[] replaceElements(int[] arr) {
+        int max = arr[arr.length - 1];
+        for (int i = arr.length - 2; i > -1; i--) {
+            int temp = max;
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            arr[i] = temp;
+        }
+        arr[arr.length - 1] = -1;
+        return arr;
+    }
+}
